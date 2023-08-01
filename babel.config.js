65ca6d1be78f7f5086f66eb05c9664aaa90ab13d
Cataldo Mazzilli <cm@zextras.com>
2023-08-01 15:27:44 +0200

    Revert "chore: unify babel config files"
    This reverts commit 0c300120aa9e4d498936f4b5666faba40f0f7efc.

--- babel.config.js
+++ babel.config.js
@@ -3,17 +3,12 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-module.exports = (api) => {
-	// You can use isTest to determine what presets and plugins to use.
-	const isTest = api.env('test');
-
-	const config = {
-		presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-		plugins: ['@babel/plugin-transform-runtime', 'babel-plugin-styled-components']
-	};
-
-	if (!isTest) {
-		config.plugins.push([
+module.exports = {
+	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+	plugins: [
+		'@babel/plugin-transform-runtime',
+		'babel-plugin-styled-components',
+		[
 			'i18next-extract',
 			{
 				outputPath: 'translations/{{ns}}.json',
@@ -21,8 +16,6 @@ module.exports = (api) => {
 				jsonSpace: 4,
 				defaultContexts: []
 			}
-		]);
-	}
-
-	return config;
+		]
+	]
 };
