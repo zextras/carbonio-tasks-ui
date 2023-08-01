0c300120aa9e4d498936f4b5666faba40f0f7efc
Cataldo Mazzilli <cm@zextras.com>
2023-08-01 11:43:35 +0200

    chore: unify babel config files

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,17 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-module.exports = {
-	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-	plugins: [
-		'@babel/plugin-transform-runtime',
-		'babel-plugin-styled-components',
-		[
+module.exports = (api) => {
+	// You can use isTest to determine what presets and plugins to use.
+	const isTest = api.env('test');
+
+	const config = {
+		presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+		plugins: ['@babel/plugin-transform-runtime', 'babel-plugin-styled-components']
+	};
+
+	if (!isTest) {
+		config.plugins.push([
 			'i18next-extract',
 			{
 				outputPath: 'translations/{{ns}}.json',
@@ -16,6 +21,8 @@ module.exports = {
 				jsonSpace: 4,
 				defaultContexts: []
 			}
-		]
-	]
+		]);
+	}
+
+	return config;
 };
--- jest.config.ts
+++ jest.config.ts
@@ -182,7 +182,7 @@ export default {
 
 	// A map from regular expressions to paths to transformers
 	transform: {
-		'^.+\\.[t|j]sx?$': ['babel-jest', { configFile: './babel.config.jest.js' }],
+		'^.+\\.[t|j]sx?$': ['babel-jest', { configFile: './babel.config.js' }],
 		'\\.(gql|graphql)$': '@graphql-tools/jest-transform',
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/src/mocks/fileTransformer.js'
