4beb2adbee792e98016f46deac4c1309436fe4bb
Cataldo Mazzilli <cm@zextras.com>
2025-04-17 18:01:50 +0200

    build: update node to version 20
    Refs: TSK-100 (#224)

--- src/jest-polyfills.ts
+++ src/jest-polyfills.ts
@@ -3,3 +3,8 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+// mock a simplified crypto
+Object.defineProperty(window.crypto, 'randomUUID', {
+	writable: true,
+	value: jest.fn(() => Math.random().toString())
+});
