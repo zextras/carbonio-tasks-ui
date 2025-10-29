23079e55a098d1866072954449de48ba52f4ce34
Cataldo Mazzilli <cm@zextras.com>
2025-10-29 15:25:30 +0100

    chore: intall @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,17 +21,24 @@ export default defineConfig({
 		})
 	],
 	test: {
-		reporters: ['verbose'],
+		reporters: process.env.CI ? ['default'] : ['verbose'],
 		retry,
 		environment: 'jsdom',
 		setupFiles: ['./src/setupTests.ts'],
 		restoreMocks: true,
+		maxWorkers: process.env.CI ? 2 : undefined,
 		coverage: {
+			enabled: true,
 			provider: 'v8',
-			reporter: ['text', 'json', 'html'],
-			exclude: ['node_modules/', 'src/setupTests.ts']
+			reporter: ['lcov', 'cobertura'],
+			exclude: ['node_modules/', 'src/setupTests.ts'],
+			thresholds: {
+				branches: 75,
+				functions: 75,
+				lines: 75,
+				statements: 75
+			}
 		},
-		maxWorkers: 2,
 		globals: true,
 		testTimeout: 60000
 	}
