ba771c0eebf6b006aa6efc5e283adc3c5a3be773
rodleyorosa <rodley.orosa@zextras.com>
2025-11-27 11:28:40 +0100

    feat: increase button size

--- src/components/ListItemHoverBar.tsx
+++ src/components/ListItemHoverBar.tsx
@@ -40,7 +40,7 @@ export const ListItemHoverBar = ({
 			padding={{ right: '0.5rem' }}
 			{...rest}
 		>
-			<CollapsingActions actions={actionsMapped} color={'text'} gap={'0.25rem'} />
+			<CollapsingActions actions={actionsMapped} size={'large'} color={'text'} gap={'0.25rem'} />
 		</HoverBarContainer>
 	);
 };
