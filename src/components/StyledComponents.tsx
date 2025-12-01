d8fdcfcd30190d05b151d6cb0a1ba0bddec274cf
Rodley <100950196+rodleyorosa@users.noreply.github.com>
2025-12-01 14:26:25 +0100

    feat: improve hovering of the lists
    Refs: CO-2114 (#295)

--- src/components/StyledComponents.tsx
+++ src/components/StyledComponents.tsx
@@ -9,12 +9,11 @@ import { Container, Row } from '@zextras/carbonio-design-system';
 
 export const HoverContainer = styled(Row)``;
 
-export const HoverBarContainer = styled(Row)`
+export const HoverBarContainer = styled(Container)`
 	display: none;
 	position: absolute;
-	top: 0;
 	right: 0;
-	background: linear-gradient(to right, transparent, currentColor);
+	background: linear-gradient(to right, transparent, currentColor 50%, currentColor 100%);
 `;
 
 export const ListItemContainer = styled(Container)`
